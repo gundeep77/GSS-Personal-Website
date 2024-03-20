b41e54561c1f12036982d9103d6654161efefd32
Gundeep Singh Saluja <gssaluja77@gmail.com>
2024-03-20 11:29:45 -0500

    fixed vscode corrupted icon

--- src/data/constants.js
+++ src/data/constants.js
@@ -178,7 +178,7 @@ export const skills = [
       {
         name: "VS Code",
         image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+          "images/vscode.png",
       },
       {
         name: "PyCharm",
